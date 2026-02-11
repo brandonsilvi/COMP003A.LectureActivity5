00b43753655c32e30737b53f3d55f8821c5d9a66
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-10 21:21:03 -0800

    method calls along with string/int declared. displayuserinfo and calc sum for 7, 8 entered

--- Program.cs
+++ Program.cs
@@ -5,9 +5,38 @@ namespace COMP003A.Assignment5
     class Program
     {
         static void Main(string[] args)
+        {
+            DisplayWelcome();  //method call
+
+            string userName = "Alex";
+            int userAge = 20;
+            DisplayUserInfo(userName, userAge);
+            int sum = CalculateSum(7, 8);
+        }
+
+
+
+
+        //methods _______
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to Module 5!");
+        }
+
+        static void DisplayUserInfo(string name, int age)
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}");
+        }
+
+        static int CalculateSum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void DisplayNumber()
+        {
+            // Console.WriteLine(number); // cannot be accessed here.
+        }
     }
-    static void DisplayWelcome()
-    {
-        Console.WriteLine("Welcome to Module 5!");
-    }
+
 }
